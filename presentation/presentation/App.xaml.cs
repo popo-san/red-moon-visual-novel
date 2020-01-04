6c0e176727141e6031944893077371ee33a4b83f
JFP <jfp.mouvement.patisserie@gmail.com>
2020-01-03 22:26:31 -0500

    Added the AppCenter logic to log crashes (exception) and updated the package properties for the projects.

--- presentation/presentation/App.xaml.cs
+++ presentation/presentation/App.xaml.cs
@@ -1,20 +1,44 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace presentation
 {
+
     public partial class App : Application
     {
+
+        const string androidKey = "9eb6f878-ea85-49d3-bbc0-cc69d4b9f20c";
+        const string uwpKey = "";
+        const string iosKey = "";
+
         public App()
         {
+
             InitializeComponent();
 
             MainPage = new MainPage();
+        
         }
 
         protected override void OnStart()
         {
+
+            // NOTE jfp See the following links for documentation.
+            // https://docs.microsoft.com/en-us/appcenter/sdk/getting-started/Xamarin
+            // https://docs.microsoft.com/en-us/appcenter/sdk/crashes/xamarin#generate-a-test-crash
+
+            AppCenter.LogLevel = LogLevel.Verbose;
+
+            AppCenter.Start($"android={androidKey};uwp={uwpKey};ios={iosKey};",
+                typeof(Analytics), typeof(Crashes));
+
+            //Crashes.GenerateTestCrash();
+
         }
 
         protected override void OnSleep()
@@ -24,5 +48,7 @@ namespace presentation
         protected override void OnResume()
         {
         }
+
     }
-}
+
+    }
